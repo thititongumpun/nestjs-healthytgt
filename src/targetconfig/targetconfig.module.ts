e161d72bf265eb250e3c01791a7f2fc50b1adeeb
thititongumpun <thiti180536@gmail.com>
2023-09-09 12:39:53 +0700

    feat: add cache response

--- src/targetconfig/targetconfig.module.ts
+++ src/targetconfig/targetconfig.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TargetconfigService } from './targetconfig.service';
 import { TargetConfigController } from './targetconfig.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [CacheModule.register()],
   providers: [TargetconfigService],
   controllers: [TargetConfigController],
 })
